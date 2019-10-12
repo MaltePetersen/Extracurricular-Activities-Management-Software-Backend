ff20f1143f0c256b70933af2e9fa364f7730d02e
MaltePetersen <petersen-malte@web.de>
2019-10-12 21:49:11 +0200

    Added verfication for UserDTO

--- src/main/java/com/main/controller/UserController.java
+++ src/main/java/com/main/controller/UserController.java
@@ -10,12 +10,15 @@ import com.main.repository.UserRepository;
 import com.main.repository.VerificationTokenRepository;
 import com.main.service.EmailService;
 import com.main.service.UserService;
+import com.main.util.UserDTOValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
@@ -24,23 +27,33 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class UserController {
 
     private UserService userService;
     private VerificationTokenRepository verificationTokenRepository;
-    UserController(UserService userService, VerificationTokenRepository verificationTokenRepository) {
+    private UserDTOValidator userDTOValidator;
+    UserController(UserService userService, VerificationTokenRepository verificationTokenRepository,UserDTOValidator userDTOValidator ) {
         this.userService = userService;
         this.verificationTokenRepository = verificationTokenRepository;
+        this.userDTOValidator = userDTOValidator;
     }
 
 
     @PostMapping("/register")
-    public ResponseEntity<String> registration(@Valid @RequestBody UserDTO user, Authentication auth) {
+    public ResponseEntity<String> registration(@RequestBody UserDTO user, Authentication auth, Errors errors) {
+        userDTOValidator.validate(user, errors);
+        if (errors.hasErrors()) {
+            return new ResponseEntity<>(createErrorString(errors), HttpStatus.BAD_REQUEST);
+        }
         return userService.save(user,auth);
     }
 
+    private String createErrorString(Errors errors) {
+        return errors.getAllErrors().stream().map(ObjectError::toString).collect(Collectors.joining(","));
+    }
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No Such Member")
     public class MemberNotFoundException extends RuntimeException {
---
+++ src/main/java/com/main/util/UserDTOValidator.java
@@ -0,0 +1,106 @@
+package com.main.util;
+
+import com.main.dto.UserDTO;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+@Component
+public class UserDTOValidator {
+
+    private boolean stringIsEmptyOrNull(String element) {
+        return element == null || element.isEmpty();
+    }
+
+    private void validateUser(UserDTO userDTO, Errors errors) {
+        validateUsername(userDTO.getUsername(), errors);
+        validatePassword(userDTO.getPassword(), errors);
+        validateFullname(userDTO.getFullname(), errors);
+    }
+
+
+    private void validateUsername(String username, Errors errors) {
+        if (stringIsEmptyOrNull(username))
+            errors.reject("No username");
+    }
+
+    private void validatePassword(String password, Errors errors) {
+        if (stringIsEmptyOrNull(password))
+            errors.reject("No password");
+    }
+
+    private void validateFullname(String fullname, Errors errors) {
+        if (stringIsEmptyOrNull(fullname))
+            errors.reject("No fullname");
+    }
+
+    private void validateEmail(String email, Errors errors) {
+        if (stringIsEmptyOrNull(email))
+            errors.reject("No email");
+    }
+
+    private void validatePhoneNumber(String phoneNumber, Errors errors) {
+        if (stringIsEmptyOrNull(phoneNumber))
+            errors.reject("No phoneNumber");
+    }
+
+    private void validateSubject(String subject, Errors errors) {
+        if (stringIsEmptyOrNull(subject))
+            errors.reject("No subject");
+    }
+
+    private void validateIban(String iban, Errors errors) {
+        if (stringIsEmptyOrNull(iban))
+            errors.reject("No iban");
+    }
+
+    private void validateSchoolClass(String schoolClass, Errors errors) {
+        if (stringIsEmptyOrNull(schoolClass))
+            errors.reject("No schoolClass");
+    }
+
+    private void validateaddress(String address, Errors errors) {
+        if (stringIsEmptyOrNull(address))
+            errors.reject("No address");
+    }
+
+
+    public void validate(UserDTO userDTO, Errors errors) {
+        switch (userDTO.getUserType()) {
+            case "USER":
+                validateUser(userDTO, errors);
+                break;
+            case "EMPLOYEE":
+                validateUser(userDTO, errors);
+                validateEmail(userDTO.getEmail(), errors);
+                validatePhoneNumber(userDTO.getPhoneNumber(), errors);
+                validateSubject(userDTO.getSubject(), errors);
+                validateIban(userDTO.getIban(), errors);
+                validateaddress(userDTO.getAddress(), errors);
+                break;
+            case "MANAGEMENT":
+                validateUser(userDTO, errors);
+                validateEmail(userDTO.getEmail(), errors);
+                validatePhoneNumber(userDTO.getPhoneNumber(), errors);
+                validateaddress(userDTO.getAddress(), errors);
+                break;
+            case "PARENT":
+                validateUser(userDTO, errors);
+                validateEmail(userDTO.getEmail(), errors);
+                validatePhoneNumber(userDTO.getPhoneNumber(), errors);
+                break;
+            case "SCHOOLCOORDINATOR":
+                validateUser(userDTO, errors);
+                break;
+            case "TEACHER":
+                validateUser(userDTO, errors);
+                validateEmail(userDTO.getEmail(), errors);
+                validatePhoneNumber(userDTO.getPhoneNumber(), errors);
+                break;
+            case "CHILD":
+                validateUser(userDTO, errors);
+                validateSchoolClass(userDTO.getSchoolClass(), errors);
+            default:
+                errors.reject("User Type is missing or wrong");
+        }
+    }
+}
