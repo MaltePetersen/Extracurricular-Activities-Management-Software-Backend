22c0cd187ab70f3c39be4a1236614cd1d35fb560
Markus Jürgens <Markus.juergens@gmx.de>
2019-10-15 12:20:23 +0200

    WIN-192

--- src/test/java/com/main/FjoerdeBackendApplicationTests.java
+++ src/test/java/com/main/FjoerdeBackendApplicationTests.java
@@ -2,15 +2,19 @@ package com.main;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DataJpaTest
 public class FjoerdeBackendApplicationTests {
-
+	
 	@Test
 	public void contextLoads() {
+	
 	}
 
 }
