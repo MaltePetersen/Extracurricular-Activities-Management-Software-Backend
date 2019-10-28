6c3ec5492bb9e14cd39302955694a3f9fceb6daa
MaltePetersen <petersen-malte@web.de>
2019-10-28 22:02:41 +0100

    Added webapp in static

--- src/main/java/com/main/FjoerdeBackendApplication.java
+++ src/main/java/com/main/FjoerdeBackendApplication.java
@@ -12,7 +12,6 @@ import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
