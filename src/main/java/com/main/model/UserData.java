de9954538d6d4f67c0395f4b7d740dc9ce3b8a80
MaltePetersen <petersen-malte@web.de>
2019-10-30 00:31:58 +0100

    added teacher example user

--- src/main/java/com/main/model/UserData.java
+++ src/main/java/com/main/model/UserData.java
@@ -57,6 +57,7 @@ public class UserData {
         users.put(teacher, "TEACHER");
         users.put(user, "USER");
         users.put(child, "CHILD");
+        users.put(parent,"PAARENT");
 		users.put(management, "MANAGEMENT");
 		return users;
 	}
