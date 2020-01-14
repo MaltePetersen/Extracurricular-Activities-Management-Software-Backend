de91bc1a5dd3e33ab56e1a9d842d125648e153cc
Bendix <bendix.tonn@outlook.de>
2020-01-14 13:58:18 +0100

    WIN-297: implemented AfterSchoolCare in ParentController

--- src/main/java/com/main/controller/ParentController.java
+++ src/main/java/com/main/controller/ParentController.java
@@ -2,9 +2,13 @@ package com.main.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.validation.constraints.Min;
 
+import com.main.dto.AfterSchoolCareDTO;
+import com.main.dto.converters.AfterSchoolCareConverter;
+import com.main.service.AfterSchoolCareService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -24,63 +28,72 @@ import com.main.model.afterSchoolCare.AfterSchoolCare;
 @RestController
 @RequestMapping("/api/parent")
 @CrossOrigin
-public class ParentController { 
+public class ParentController {
+    AfterSchoolCareService afterSchoolCareService;
     List<UserDTO> childs;
-    ParentController(){
-        generateChildren(); 
+
+    ParentController(AfterSchoolCareService afterSchoolCareService) {
+        this.afterSchoolCareService = afterSchoolCareService;
+        generateChildren();
     }
-    private void generateChildren(){
+
+    private void generateChildren() {
         childs = new ArrayList<>();
         for (int i = 1; i < 10; i++) {
-            UserDTO child = UserDTO.builder().address("Adresse" + i).fullname("Kind "+ i).userType("ROLE_CHILD").schoolClass(i + "b").username("Kind " + i).build();
+            UserDTO child = UserDTO.builder().address("Adresse" + i).fullname("Kind " + i).userType("ROLE_CHILD").schoolClass(i + "b").username("Kind " + i).build();
             childs.add(child);
         }
     }
-	@GetMapping("/authority")
-	public ResponseEntity<Void> isParent(Authentication auth) {
-		if (auth != null) {
-			List<String> roles = new ArrayList<>();
-			if (auth != null)
-				auth.getAuthorities().forEach((a) -> {
-					roles.add(a.getAuthority());
-				});
-			if(roles.contains("ROLE_PARENT")) {
-				return new ResponseEntity<Void>(HttpStatus.ACCEPTED);
-			}
 
-		}
-		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-	}
-	
-@GetMapping("/bookedafterschoolcares")
-	public String getBookedAfterSchoolCares(){
-		return "Not yet implemented";
-	}
+    @GetMapping("/authority")
+    public ResponseEntity<Void> isParent(Authentication auth) {
+        if (auth != null) {
+            List<String> roles = new ArrayList<>();
+            if (auth != null)
+                auth.getAuthorities().forEach((a) -> {
+                    roles.add(a.getAuthority());
+                });
+            if (roles.contains("ROLE_PARENT")) {
+                return new ResponseEntity<Void>(HttpStatus.ACCEPTED);
+            }
+
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/bookedafterschoolcares")
+    public String getBookedAfterSchoolCares() {
+        return "Not yet implemented";
+    }
+
     @PostMapping("/bookedafterschoolcare")
     @ResponseStatus(HttpStatus.CREATED)
     String createBookedAfterSchoolCare(@RequestBody AfterSchoolCare AfterSchoolCare) {
         return "Not yet implemented";
     }
+
     @PatchMapping("/bookedafterschoolcare/{id}")
     String changeBookedAfterSchoolCare(@RequestBody AfterSchoolCare AfterSchoolCare, @PathVariable Long id) {
         return "Not yet implemented";
     }
+
     @DeleteMapping("/bookedafterschoolcare/{id}")
     String deleteBookedAfterSchoolCare(@PathVariable Long id) {
         return "Not yet implemented";
-	}
-	
-	@GetMapping("/afterschoolcares")
-	public String getAfterSchoolCares(){
-		return "Not yet implemented";
-	}
-	
+    }
+
+    @GetMapping("/afterschoolcares")
+    public List<AfterSchoolCareDTO> getAfterSchoolCares() {
+        return afterSchoolCareService.getAll().stream().map(AfterSchoolCareConverter::toDto)
+                .collect(Collectors.toList());
+    }
+
     @GetMapping("/afterschoolcare/{id}")
-    public String getAfterSchoolCare(@PathVariable int id) {
-        return "Not yet implemented";
-	}
-	
-	@GetMapping("/childs")
+    public AfterSchoolCareDTO getAfterSchoolCare(@PathVariable Long id) {
+        return AfterSchoolCareConverter.toDto(afterSchoolCareService.findOne(id));
+    }
+
+    @GetMapping("/childs")
     public List<UserDTO> getChilds() {
         return childs;
     }
@@ -93,18 +106,18 @@ public class ParentController {
 
     @GetMapping("/child/{id}")
     UserDTO getChild(@PathVariable @Min(1) int id) {
-        return childs.get(id-1);
+        return childs.get(id - 1);
     }
 
     @PatchMapping("/child/{id}")
     void changeChild(@RequestBody UserDTO child, @PathVariable int id) {
-        childs.set(id -1, child);
+        childs.set(id - 1, child);
 
     }
 
     @DeleteMapping("/child/{id}")
     void deleteChild(@PathVariable int id) {
-        childs.remove(id -1);
+        childs.remove(id - 1);
     }
-		
+
 }
