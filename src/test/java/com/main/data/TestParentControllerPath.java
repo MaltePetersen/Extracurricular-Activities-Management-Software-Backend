a7e6a391ae31d0eef950f451bb8328c02389c375
Bendix <bendix.tonn@outlook.de>
2020-02-04 17:10:15 +0100

    WIN-390 - ParentController: added unit test for get types

--- src/test/java/com/main/data/TestParentControllerPath.java
+++ src/test/java/com/main/data/TestParentControllerPath.java
@@ -2,7 +2,7 @@ package com.main.data;
 
 public enum TestParentControllerPath {
 	ADDCHILD("/addChild"), SCHOOLS("/api/parent/schools"), AFTER_SCHOOL_CARES("/api/parent/after_school_cares"), CREATE_CHILD("/api/parent/child"),
-	CHILDREN("/api/parent/children"), UPDATE_CHILD("/api/parent//child/");
+	CHILDREN("/api/parent/children"), UPDATE_CHILD("/api/parent//child/"), GET_TYPES("/api/parent/after_school_cares/types");
 	private String path;
 	
 	public String getUri() {
--- src/test/java/com/main/mockmvc/ParentControllerTest.java
+++ src/test/java/com/main/mockmvc/ParentControllerTest.java
@@ -2,12 +2,13 @@ package com.main.mockmvc;
 
 import com.main.data.TestParentControllerPath;
 import com.main.dto.AfterSchoolCareDTO;
-import com.main.model.Attendance;
+import com.main.model.afterSchoolCare.AfterSchoolCare;
 import com.main.model.afterSchoolCare.AfternoonCare;
 import com.main.repository.UserRepository;
 import com.main.service.AfterSchoolCareService;
 import com.main.service.AttendanceService;
 import lombok.extern.java.Log;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,99 +17,123 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.time.LocalDateTime;
-
 import static org.junit.Assert.assertEquals;
 
 @Log
 public class ParentControllerTest extends AbstractMvcTest {
-	private AfternoonCare testAfternoonCare;
+    private AfternoonCare testAfternoonCare;
 
-	@Autowired
-	private AfterSchoolCareService afterSchoolCareService;
+    @Autowired
+    private AfterSchoolCareService afterSchoolCareService;
 
-	@Autowired
-	private AttendanceService attendanceService;
+    @Autowired
+    private AttendanceService attendanceService;
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Override
-	@Before
-	public void setUp() {
-		testAfternoonCare = new AfternoonCare();
-		testAfternoonCare.setName("Test-Nachmittagsbetreuung");
-		testAfternoonCare.setOwner(userRepository.findByUsername("Employee_Test"));
-		afterSchoolCareService.save(testAfternoonCare);
+    @Override
+    @Before
+    public void setUp() {
+        testAfternoonCare = new AfternoonCare();
+        testAfternoonCare.setName("Test-Nachmittagsbetreuung");
+        testAfternoonCare.setOwner(userRepository.findByUsername("Employee_Test"));
+        afterSchoolCareService.save(testAfternoonCare);
 
-		super.setUp();
-	}
+        super.setUp();
+    }
 
-	@Test
-	@WithMockUser(authorities = "ROLE_PARENT")
-	public void testGetParentWithAuthorityParent() throws Exception {
-		String uri = "/api/parent/authority";
-		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
-		assertEquals(202, mvcResult.getResponse().getStatus());
-	}
+    @Test
+    @WithMockUser(authorities = "ROLE_PARENT")
+    public void testGetParentWithAuthorityParent() throws Exception {
+        String uri = "/api/parent/authority";
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
+        assertEquals(202, mvcResult.getResponse().getStatus());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void testGetParentWithoutAuthorities() throws Exception {
+        String uri = "/api/parent/authority";
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
+        assertEquals(401, mvcResult.getResponse().getStatus());
+    }
+
+    @Test
+    @WithMockUser(authorities = "ROLE_PARENT")
+    public void testGetSchoolsWithParentAuthority() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(TestParentControllerPath.SCHOOLS.getUri())).andReturn();
+        assertEquals(200, mvcResult.getResponse().getStatus());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void testGetSchoolsWithoutAuthorities() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(TestParentControllerPath.SCHOOLS.getUri())).andReturn();
+        assertEquals(401, mvcResult.getResponse().getStatus());
+    }
+
+    @Test
+    @WithMockUser(authorities = "ROLE_PARENT")
+    public void testGetAfternoonCaresWithParentAuthority() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(TestParentControllerPath.AFTER_SCHOOL_CARES.getUri())).andReturn();
+        assertEquals(200, mvcResult.getResponse().getStatus());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void testGetAfternoonCaresWithoutAuthorities() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(TestParentControllerPath.AFTER_SCHOOL_CARES.getUri())).andReturn();
+        assertEquals(401, mvcResult.getResponse().getStatus());
+    }
+
+    @Test
+    @WithMockUser(authorities = "ROLE_PARENT")
+    public void testGetSingleAfternoonCareWithParentAuthority() throws Exception {
+        String uri = TestParentControllerPath.AFTER_SCHOOL_CARES.getUri() + "/" + testAfternoonCare.getId();
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
+
+        assertEquals(200, mvcResult.getResponse().getStatus());
+
+        String content = mvcResult.getResponse().getContentAsString();
+        AfterSchoolCareDTO resultAfternoonCare = objectMapper.readValue(content, AfterSchoolCareDTO.class);
+
+        assertEquals(testAfternoonCare.getId(), resultAfternoonCare.getId());
+        assertEquals(testAfternoonCare.getName(), resultAfternoonCare.getName());
+        assertEquals(testAfternoonCare.getOwner().getUsername(), resultAfternoonCare.getOwner().getUsername());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void testGetSingleAfternoonCareWithoutAuthorities() throws Exception {
+        String uri = TestParentControllerPath.AFTER_SCHOOL_CARES.getUri() + "/" + testAfternoonCare.getId();
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
+        assertEquals(401, mvcResult.getResponse().getStatus());
+    }
+
+    @Test
+    @WithMockUser(authorities = "ROLE_PARENT")
+    public void testGetAfterSchoolCareTypesWithParentAuthority() throws Exception {
+        String uri = TestParentControllerPath.GET_TYPES.getUri();
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
+
+        assertEquals(200, mvcResult.getResponse().getStatus());
+
+        String content = mvcResult.getResponse().getContentAsString();
+		JSONObject result = new JSONObject(content);
+
+        assertEquals(result.get(Integer.toString(AfterSchoolCare.Type.AFTERNOON_CARE.getId())), AfterSchoolCare.getTypes().get(AfterSchoolCare.Type.AFTERNOON_CARE.getId()));
+        assertEquals(result.get(Integer.toString(AfterSchoolCare.Type.WORKING_GROUP.getId())), AfterSchoolCare.getTypes().get(AfterSchoolCare.Type.WORKING_GROUP.getId()));
+        assertEquals(result.get(Integer.toString(AfterSchoolCare.Type.REMEDIAL.getId())), AfterSchoolCare.getTypes().get(AfterSchoolCare.Type.REMEDIAL.getId()));
+        assertEquals(result.get(Integer.toString(AfterSchoolCare.Type.AMPLIFICATION.getId())), AfterSchoolCare.getTypes().get(AfterSchoolCare.Type.AMPLIFICATION.getId()));
+    }
 
 	@Test
 	@WithAnonymousUser
-	public void testGetParentWithoutAuthorities() throws Exception {
-		String uri = "/api/parent/authority";
-		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
-		assertEquals(401, mvcResult.getResponse().getStatus());
-	}
-
-	@Test
-	@WithMockUser(authorities = "ROLE_PARENT")
-	public void testGetSchoolsWithParentAuthority() throws Exception {
-		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(TestParentControllerPath.SCHOOLS.getUri())).andReturn();
-		assertEquals(200, mvcResult.getResponse().getStatus());
-	}
-
-	@Test
-	@WithAnonymousUser
-	public void testGetSchoolsWithoutAuthorities() throws Exception {
-		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(TestParentControllerPath.SCHOOLS.getUri())).andReturn();
-		assertEquals(401, mvcResult.getResponse().getStatus());
-	}
-
-	@Test
-	@WithMockUser(authorities = "ROLE_PARENT")
-	public void testGetAfternoonCaresWithParentAuthority() throws Exception {
-		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(TestParentControllerPath.AFTER_SCHOOL_CARES.getUri())).andReturn();
-		assertEquals(200, mvcResult.getResponse().getStatus());
-	}
-
-	@Test
-	@WithAnonymousUser
-	public void testGetAfternoonCaresWithoutAuthorities() throws Exception {
-		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(TestParentControllerPath.AFTER_SCHOOL_CARES.getUri())).andReturn();
-		assertEquals(401, mvcResult.getResponse().getStatus());
-	}
-
-	@Test
-	@WithMockUser(authorities = "ROLE_PARENT")
-	public void testGetSingleAfternoonCareWithParentAuthority() throws Exception {
-		String uri = TestParentControllerPath.AFTER_SCHOOL_CARES.getUri() + "/" + testAfternoonCare.getId();
+	public void testGetAfterSchoolCareTypesWithoutAuthorities() throws Exception {
+		String uri = TestParentControllerPath.GET_TYPES.getUri();
 		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
 
-		assertEquals(200, mvcResult.getResponse().getStatus());
-
-		String content = mvcResult.getResponse().getContentAsString();
-		AfterSchoolCareDTO resultAfternoonCare = objectMapper.readValue(content, AfterSchoolCareDTO.class);
-
-		assertEquals(testAfternoonCare.getId(), resultAfternoonCare.getId());
-		assertEquals(testAfternoonCare.getName(), resultAfternoonCare.getName());
-		assertEquals(testAfternoonCare.getOwner().getUsername(), resultAfternoonCare.getOwner().getUsername());
-	}
-
-	@Test
-	@WithAnonymousUser
-	public void testGetSingleAfternoonCareWithoutAuthorities() throws Exception {
-		String uri = TestParentControllerPath.AFTER_SCHOOL_CARES.getUri() + "/" + testAfternoonCare.getId();
-		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
 		assertEquals(401, mvcResult.getResponse().getStatus());
 	}
 }
